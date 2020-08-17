ac76ef81f441719e6c4ba0d18f595b12773ff7e3
Cloe Furlan <cloe.furlan@yahoo.fr>
2020-08-17 16:22:34 -0400

    Init cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :equipments, :bookings
-  
+  has_many :equipments
+  has_many :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
