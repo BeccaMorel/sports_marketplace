00b2394165359d8a53e04787d26e42bdad978cea
Rebecca Morel <rebecca.morel@mail.mcgill.ca>
2020-08-20 17:48:14 -0400

    change in validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :equipment
   belongs_to :user
   validates :date, presence: true
-  validates_uniqueness_of :equipment_id, scope: :user_id
+  validates_uniqueness_of :equipment_id, scope: :user_id, condition: -> { where(status: "in pending")}
 end
