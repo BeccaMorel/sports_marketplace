aa35982d2b5f7fd773350e443e0a6bd863d99254
Rebecca Morel <rebecca.morel@mail.mcgill.ca>
2020-08-20 17:58:52 -0400

    validation of

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,4 @@ class Booking < ApplicationRecord
   belongs_to :equipment
   belongs_to :user
   validates :date, presence: true
-  validates_uniqueness_of :equipment_id, scope: :user_id, condition: -> { where(status: "in pending")}
 end
