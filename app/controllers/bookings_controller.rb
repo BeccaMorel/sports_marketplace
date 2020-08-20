412fa61a5a134625505751c65cdee1dc8117dbba
Rebecca Morel <rebecca.morel@mail.mcgill.ca>
2020-08-20 14:11:59 -0400

    first try (not working yet)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -39,9 +39,7 @@ class BookingsController < ApplicationController
 
     @booking.equipment = @equipment
     @booking.user = current_user
-
     authorize(@booking)
-
     if @booking.save
       redirect_to equipment_path(@equipment)
     else
